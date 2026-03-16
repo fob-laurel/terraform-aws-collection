6b9e7320f62556b548a6141c92a3ec58f6953585
Francisco Obando <francisco@laurel.ai>
2026-03-16 15:44:16 -0700

    fix: replace deprecated data.aws_region.current.name with .region in forwarder/queue.tf

--- modules/forwarder/queue.tf
+++ modules/forwarder/queue.tf
@@ -1,6 +1,6 @@
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.name
+  region     = data.aws_region.current.region
   sqs_arn    = "arn:${data.aws_partition.current.id}:sqs:${local.region}:${local.account_id}:${var.name}"
   enable_dlq = var.queue_max_receive_count > 0
 }
